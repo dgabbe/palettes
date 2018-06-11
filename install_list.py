28b6b0d01a3770c4b67e7c904ad61e99b94fad9d
David G <dgabbe@acm.org>
2018-06-11 07:57:04 -0400

    inkscape palettes installer. Finished LinkedIn palette.

---
+++ install_list.py
@@ -0,0 +1,7 @@
+# _palettes inkscape and gimp
+scripts = [
+  [".config/inkscape/palettes/flat-colors.gpl", "flat-colors.gpl"],
+  [".config/inkscape/palettes/LinkedIn-screen.gpl", "LinkedIn-screen.gpl"],
+  [".config/inkscape/palettes/material-colors.gpl", "material-colors.gpl"],
+  [".config/inkscape/palettes/social-ui-colors.gpl", "social-ui-colors.gpl"]
+]
